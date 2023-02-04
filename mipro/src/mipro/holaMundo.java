75e3f067589fa552c8ebe7d1b73e1556a08c8628
gabgc <gabgc@debian>
2023-02-04 21:20:10 +0100

    Primer commit

---
+++ mipro/src/mipro/holaMundo.java
@@ -0,0 +1,13 @@
+package mipro;
+
+public class holaMundo {
+	
+	
+	public static void main (String[] args) {
+		int a = 2;
+		int b = 3;
+		int resultado = a+b;
+		System.out.println("Hello world, Hola mundo: " +resultado);
+	}
+
+}
