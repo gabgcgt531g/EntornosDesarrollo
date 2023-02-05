69405f94de85dbae5153ee4c9b9a4955d100cb44
gabgc <gabgc@debian>
2023-02-05 12:07:22 +0100

    consolidado

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -2,11 +2,10 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
-		System.out.println("Modificación desde nano");
-
-		System.out.println("2 cambio - Eclipse");
-		System.out.println("2.1 cambio - consola");
-
+		System.out.println("1 cambio - Consola");
+		System.out.println("1 cambio - Eclipse");
+		
+		System.out.println("2.1 cambio - Consola");
 		System.out.println("2.1 cambio - Eclipse");
 	}
 
