f915e95d846ec0e1fe44a1bf3522aaeced89cfdc
root <gabgc.z930@gmail.com>
2023-02-05 10:14:18 +0100

    Cambio desde nano

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -4,6 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 		System.out.println("Hola mundo");
+		System.out.println("Modificación desde nano");
 	}
 
 }
