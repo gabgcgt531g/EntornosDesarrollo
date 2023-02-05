f1efaaacb3f140ba88862727fa470615872d3b12
gabgc <gabgc@debian>
2023-02-05 12:13:21 +0100

    3 cambio eclipse

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -4,9 +4,12 @@ public class HolaMundo {
 		// TODO Esbozo de método generado automáticamente
 		System.out.println("1 cambio - Consola");
 		System.out.println("1 cambio - Eclipse");
-		
-		System.out.println("2.1 cambio - Consola");
-		System.out.println("2.1 cambio - Eclipse");
+
+		System.out.println("2 cambio - Consola");
+		System.out.println("2 cambio - Eclipse");
+
+		System.out.println("3 cambio - Consola");
+		System.out.println("3 cambio - Eclipse");
 	}
 
 }
