554bda29b8293e7ff6d212519bb86399681f9e11
root <gabgc.z930@gmail.com>
2023-02-05 10:43:01 +0100

    1er cambio - consola

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -4,9 +4,8 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 		System.out.println("Modificación desde nano");
-=======
+
 		System.out.println("l cambio - Eclipse");
->>>>>>> bca4cd8b10947bce56c6cae12b3591be2d6b8935
 	}
 
 }
