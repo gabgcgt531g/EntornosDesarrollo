29112040920f7bf50e28e057771632a9127310e5
gabgc <gabgc@debian>
2023-02-05 12:00:40 +0100

    ultimo cambio eclipse

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 		System.out.println("Modificación desde nano");
-		System.out.println("2 cambio - Eclipse");
+		System.out.println("2.1 cambio - Eclipse");
 
 	}
 
